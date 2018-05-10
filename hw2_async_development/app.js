3748a621467e7228346d0060ab256eabed9175a2
Aleksandr Eremin <aleksandr.eremin@thomsonreuters.com>
2018-05-10 18:50:42 +0300

    [init] isMapSame

--- hw2_async_development/app.js
+++ hw2_async_development/app.js
@@ -9,7 +9,6 @@ const WATCH_DIR = './data';
 const WATCH_DELAY = 5000;
 const DIRWATCH_EVENT_NAME = 'dirwatcher:changed';
 
-
 const requestHandler = (request, response) => {
     console.log(request.url);
     response.end('Hello Node.js Server!');
@@ -20,16 +19,12 @@ server.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
-    console.log(`server is listening on ${port}`);
 
-    // Import all of the above modules.
-    // Create a Dirwatcher and Importer for processing files asynchronously from data directory.
-    // Log imported data to console.
     const dirWatcher = new DirWatcher(DIRWATCH_EVENT_NAME);
     dirWatcher.watch(WATCH_DIR, WATCH_DELAY);
 
     const importer = new Importer(dirWatcher);
     importer.import(WATCH_DIR);
 
-    console.log('123');
+    console.log(`server is listening on ${port}`);
 });
--- hw2_async_development/dirwatcher/DirWatcher.js
+++ hw2_async_development/dirwatcher/DirWatcher.js
@@ -13,6 +13,7 @@ export default class {
         this.myEmitter = new MyEmitter();
         this.files = new Map();
         this.intervalListeners = new Map();
+        this.onDirWatchAction = new Map();
     }
 
     /**
@@ -27,14 +28,28 @@ export default class {
         const onDirWatchAction = () => {
             console.log('DirWatcher: an event occurred!');
         };
+        this.onDirWatchAction.set(this.getWatcherName(path, delay), onDirWatchAction);
         this.myEmitter.on(this.eventName, onDirWatchAction);
 
         const readDirCallback = (err, files) => {
             if (err) throw new Error(err);
 
-            console.log('files >>> ', files);
+            let mapFiles = new Map();
+            files.forEach(item => {
+                mapFiles.set(item, item);
+            });
+            if (this.files.size === 0 || !this.isMapSame(this.files, mapFiles)) {
+                console.log('files >>> ', files);
 
-            this.myEmitter.emit(this.eventName);
+                files.forEach(item => {
+                    this.files.set(item, item);
+                });
+
+                this.myEmitter.emit(this.eventName);
+            } else {
+                console.log('files >>> ', 'the same...');
+            }
+
         };
 
         fs.readdir(path || this.path, {encoding: 'utf8'}, readDirCallback);
@@ -42,17 +57,42 @@ export default class {
         const intervalListener = setInterval(() => {
             fs.readdir(path || this.path, {encoding: 'utf8'}, readDirCallback);
         }, delay);
-        this.intervalListeners.set(this.getIntervalName(path, delay), intervalListener);
+        this.intervalListeners.set(this.getWatcherName(path, delay), intervalListener);
+    }
+
+    isMapSame(map1, map2) {
+        if (map1 && map2 && map1.size !== map2.size) {
+            return false;
+        }
+
+        let isSame = true;
+
+        for (var [key, value] of map1) {
+            if (!map2.has(key)) {
+                isSame = false;
+            }
+        }
+        for (var [key, value] of map2) {
+            if (!map1.has(key)) {
+                isSame = false;
+            }
+        }
+
+        return isSame;
     }
 
     unwatch(path, delay) {
-        this.intervalListeners.has(this.getIntervalName(path, delay))
-        && this.intervalListeners.get(this.getIntervalName(path, delay))();
+        this.intervalListeners.has(this.getWatcherName(path, delay))
+        && this.intervalListeners.get(this.getWatcherName(path, delay))();
 
-        this.myEmitter.removeListener(eventName, listener)
+        this.onDirWatchAction.has(this.getWatcherName(path, delay))
+        && this.myEmitter.removeListener(
+            this.eventName,
+            this.onDirWatchAction.get(this.getWatcherName(path, delay))
+        );
     }
 
-    getIntervalName(path, delay) {
+    getWatcherName(path, delay) {
         return `${path || ''}_${delay || ''}`;
     }
 }
