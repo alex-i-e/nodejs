67108caca789b626fe311f76003ee92383dc83b3
Alex <aleksandr.i.eremin@yandex.ru>
2018-05-11 09:00:37 +0300

    [init] importSync

--- hw2_async_development/app.js
+++ hw2_async_development/app.js
@@ -1,30 +1,31 @@
-// import "babel-register";
 import DirWatcher from "./dirwatcher/DirWatcher";
 import Importer from "./importer/Importer";
+const path = require('path');
 
 
 const http = require('http');
 const port = 3000;
-const WATCH_DIR = './data';
+const WATCH_DIR = 'data/';
 const WATCH_DELAY = 5000;
 const DIRWATCH_EVENT_NAME = 'dirwatcher:changed';
 
 const requestHandler = (request, response) => {
-    console.log(request.url);
-    response.end('Hello Node.js Server!');
+  console.log(request.url);
+  response.end('Hello Node.js Server!');
 };
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err);
-    }
+  if (err) {
+    return console.log('something bad happened', err);
+  }
 
-    const dirWatcher = new DirWatcher(DIRWATCH_EVENT_NAME);
-    dirWatcher.watch(WATCH_DIR, WATCH_DELAY);
+  const dirWatcher = new DirWatcher(DIRWATCH_EVENT_NAME);
+  dirWatcher.watch(WATCH_DIR, WATCH_DELAY);
 
-    const importer = new Importer(dirWatcher);
-    importer.import(WATCH_DIR);
+  const importer = new Importer(dirWatcher);
+  importer.import(path.join(__dirname, WATCH_DIR));
+  importer.importSync(path.join(__dirname, WATCH_DIR));
 
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
--- hw2_async_development/importer/Importer.js
+++ hw2_async_development/importer/Importer.js
@@ -1,32 +1,46 @@
+const csvjson = require('csvjson');
+const fs = require('fs');
+const path = require('path');
+
 export default class {
-    constructor(dirwatcher) {
-        this.dirwatcher = dirwatcher;
-    }
+  constructor(dirwatcher) {
+    this.dirwatcher = dirwatcher;
+  }
 
-    // * It should be able to listen to DirWatcher events
-    // * and start importing CSV files
-    // * (converting the data to JavaScript objects) on ‘dirwatcher:changed’ event.
-    // should return a promise with imported data from file at path
-    import(path, eventName) {
+  // * It should be able to listen to DirWatcher events
+  // * and start importing CSV files
+  // * (converting the data to JavaScript objects) on ‘dirwatcher:changed’ event.
+  // should return a promise with imported data from file at path
+  import(dataPath) {
 
-        const onAsyncImportAction = (a, b) => {
-            setImmediate(() => {
-                console.log('Importer: this happens Asynchronously');
-            });
+    const onAsyncImportAction = (a, b) => {
+      setImmediate(() => {
+        console.log('Importer: this happens Asynchronously...');
+      });
+    };
+
+    console.log('this.dirwatcher=', this.dirwatcher);
+    this.dirwatcher.myEmitter.on(this.dirwatcher.eventName, onAsyncImportAction);
+  }
+
+  // should be synchronous and return all imported data from file at path.
+  importSync(dataPath) {
+
+    // set function to avoid emitter leak in the end
+    const onSyncImportAction = () => {
+      console.log('Importer: this happens Synchronously...');
+
+      this.dirwatcher.files.forEach(item => {
+        const data = fs.readFileSync(dataPath + item, { encoding: 'utf8' });
+        const options = {
+          delimiter: ',',
+          quote: '"'
         };
 
-        console.log('this.dirwatcher=', this.dirwatcher);
-        this.dirwatcher.myEmitter.on(this.dirwatcher.eventName, onAsyncImportAction);
-    }
+        console.log(csvjson.toObject(data, options));
+      });
+    };
 
-    // should be synchronous and return all imported data from file at path.
-    importSync(path) {
-
-        // set function to avoid emitter leak in the end
-        const onSyncImportAction = () => {
-            console.log('Importer: this happens Synchronously');
-        };
-
-        this.dirwatcher.myEmitter.on(this.dirwatcher.eventName, onSyncImportAction);
-    }
+    this.dirwatcher.myEmitter.on(this.dirwatcher.eventName, onSyncImportAction);
+  }
 }
