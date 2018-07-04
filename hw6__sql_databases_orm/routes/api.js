8927853c72f8510b45acaa75c36a933e79dcd5d1
Aleksandr Eremin <aleksandr.eremin@thomsonreuters.com>
2018-07-04 14:55:00 +0300

    [hw6] make generic query for error processing

--- hw6__sql_databases_orm/routes/api.js
+++ hw6__sql_databases_orm/routes/api.js
@@ -1,50 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
+async function processQuery(dbClient, queryString, res) {
+    try {
+        const query = await dbClient.query(queryString);
+
+        const rowsJson = JSON.stringify(query.rows);
+        console.log('rowsJson >>> ', rowsJson);
+
+        res.json({
+            status: "OK",
+            result: query.rows
+        });
+    } catch (err) {
+        res.json({
+            status: "ERROR",
+            code: err.statusCode,
+            result: err.message
+        });
+    }
+}
+
 module.exports = function processApiRoutes(dbClient) {
     router.get('/products', async function getAllProducts(req, res) {
-
-        const query = await
-            dbClient.query(`
-    SELECT * 
-    FROM "Products"
-    ORDER BY id ASC 
-    `);
-
-
-        const rowsJson = JSON.stringify(query.rows);
-        console.log('rowsJson >>> ', rowsJson);
-
-        res.json({
-            status: "OK",
-            result: query.rows
-        });
+        await processQuery(
+            dbClient, `
+            SELECT * 
+            FROM "Products"
+            ORDER BY id ASC 
+            `,
+            res);
     });
+
     router.get('/products/:id', async function getSpecifiedProduct(req, res) {
-
-        const query = await
-            dbClient.query(`
-    SELECT * 
-    FROM "Products" as pr
-    WHERE pr.id = ${req.params.id}
-    ORDER BY id ASC 
-    `);
-
-        const rowsJson = JSON.stringify(query.rows);
-        console.log('rowsJson >>> ', rowsJson);
-
-        res.json({
-            status: "OK",
-            result: query.rows
-        });
+        await processQuery(
+            dbClient, `
+            SELECT * 
+            FROM "Products" as pr
+            WHERE pr.id = ${req.params.id}
+            ORDER BY id ASC 
+            `,
+            res);
     });
+
     router.get('/products/:id/reviews', function getReviewBySpecifiedProduct(req, res) {
         res.send(`Return review from the product with id: ${req.params.id}`);
     });
-    router.post('/products', async function addProduct(req, res) {
 
-        const query = await dbClient.query(`
-        INSERT INTO "Products" (name, brand, price, options, "createdAt", "updatedAt") VALUES
+    router.post('/products', async function addProduct(req, res) {
+        await processQuery(
+            dbClient, `
+            INSERT INTO "Products" (name, brand, price, options, "createdAt", "updatedAt") VALUES
             (
             '${req.body.name}', 
             '${req.body.brand}', 
@@ -53,34 +59,21 @@ module.exports = function processApiRoutes(dbClient) {
             '${new Date().toUTCString()}', 
             '${new Date().toUTCString()}'
             )
-        RETURNING *
-        `);
-
-
-        const rowsJson = JSON.stringify(query.rows);
-        console.log('rowsJson >>> ', rowsJson);
-
-        res.json({
-            status: "OK",
-            result: query.rows
-        });
+            RETURNING *
+            `,
+            res);
     });
+
     router.get('/users', async function getAllUsers(req, res) {
-
-        const query = await dbClient.query(`
-    SELECT * 
-    FROM "Users" as pr
-    ORDER BY id ASC 
-    `);
-
-        const rowsJson = JSON.stringify(query.rows);
-        console.log('rowsJson >>> ', rowsJson);
-
-        res.json({
-            status: "OK",
-            result: query.rows
-        });
+        await processQuery(
+            dbClient, `
+            SELECT * 
+            FROM "Users" as pr
+            ORDER BY id ASC 
+            `,
+            res);
     });
+
     router.get('/*', function throwError(req, res, next) {
         next(new Error('API does not exist yet!'));
     });
